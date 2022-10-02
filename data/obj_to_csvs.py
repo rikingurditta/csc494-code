3e60874c213cd87e3702b4881cd4025b6edef56b
Rikin Gurditta <dolllyamar@gmail.com>
2022-10-02 01:58:37 -0400

    Fixed coordinates taken for V and index +1 error for E in Python script

--- data/obj_to_csvs.py
+++ data/obj_to_csvs.py
@@ -7,9 +7,9 @@ def obj_to_csvs(file):
     for line in map(lambda x: x.strip('\n'), filter(lambda x: len(x.split(' ')) >= 3, file.readlines())):
         entries = line.split(' ')
         if entries[0] == 'v':
-            v.append(f'{entries[1]} {entries[2]}')
+            v.append(f'{entries[1]},{entries[3]}')
         elif entries[0] == 'l':
-            e.append(f'{entries[1]} {entries[2]}')
+            e.append(f'{int(entries[1]) - 1},{int(entries[2]) - 1}')
     with open(f'{splitext(basename(file.name))[0]}_V.csv', 'w') as v_file:
         v_file.write('\n'.join(v))
     with open(f'{splitext(basename(file.name))[0]}_E.csv', 'w') as e_file:
